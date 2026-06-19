fedc810da72b3fdfb5d441f2f15fba5902e3c8b4
Clara Nascimento <clara.nasc13@gmail.com>
2026-06-19 13:14:01 -0300

    feat: create DeliveriesController to handle creation and listing of deliveries

--- src/controllers/deliveries-controller.ts
+++ src/controllers/deliveries-controller.ts
@@ -4,34 +4,41 @@ import { z } from "zod";
 
 //classe Responsavel por cadastrar entregas
 class DeliveriesController {
-    async create(request: Request, response: Response) {
-        const bodySchema = z.object({
-            user_id: z.string().uuid(),
-            description: z.string()
-        })
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      user_id: z.string().uuid(),
+      description: z.string(),
+    });
 
-        const {
-            user_id,
-            description
-        } = bodySchema.parse(request.body)
+    const { user_id, description } = bodySchema.parse(request.body);
 
-        //isso serve para garantir que o usuário existe antes de criar a entrega
-        await prisma.delivery.create({
-            data: {
-                userId: user_id,
-                description: description
-            }
-        })
+    //isso serve para garantir que o usuário existe antes de criar a entrega
+    await prisma.delivery.create({
+      data: {
+        userId: user_id,
+        description: description,
+      },
+    });
 
-        return response.status(201).send();
-    }
+    return response.status(201).send();
+  }
 
-    //listar entregas
-    async index(request: Request, response: Response) {
-        const deliveries = await prisma.delivery.findMany();
+  //listar entregas
+  async index(request: Request, response: Response) {
+    const deliveries = await prisma.delivery.findMany({
+      //inclui informações na response como name e email 
+        include: {
+        user: {
+          select: {
+            name: true,
+            email: true,
+          },
+        },
+      },
+    });
 
-        return response.json(deliveries);
-    }
+    return response.json(deliveries);
+  }
 }
 
 export { DeliveriesController };
